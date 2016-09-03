2f3bbd010fe690300b2efb1de7c7334d39d60aa2
Saya <hello@iamsaya.com>
2016-09-03 23:57:21 +0900

    update : QLearningAgent.py

--- smartcab/QLearningAgent.py
+++ smartcab/QLearningAgent.py
@@ -3,6 +3,7 @@ from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
 
+
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
@@ -10,74 +11,65 @@ class LearningAgent(Agent):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
-        # TODO: Initialize any additional variables here
         self.dedline = self.env.get_deadline(self)
         self.possible_action = Environment.valid_actions
-        self.Q_dict = dict()
-        self.epsilon = 0.05
-        self.alpha = 0.5
-        self.gamma = 0.3
+        self.Q_dict = dict() # Q(s,a)
+
         self.state = None
         self.action = None
-        self.next_waypoint = None
         self.reward = None
         self.cum_rewards = 0
+        self.next_waypoint = None
+
+        self.epsilon = 0.2 # Pick a random action 20% of the time.
+        self.alpha = 0.8 # learning rate
+        self.gamma = 0.5 # discount maxQ(s',a')
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
-        # TODO: Prepare for a new trip; reset any variables here, if required
-        self.next_waypoint = None
-
         self.state = None
         self.next_state = None
-
         self.action = None
-
+        self.next_action = None
         self.reward = 0
         self.cum_rewards = 0
+        self.next_waypoint = None
 
     def get_Q(self, state, action):
+        # Q value (s,a)
         key = (state, action)
         return self.Q_dict.get(key, 10.0)
 
     def get_maxQ(self, state):
-        """
-        Returns maxQ(state,action)
-        """
-        best_Q = [self.get_Q(state, action) for action in self.possible_action]
-        return max(best_Q)
+        # Returns maxQ(s,a)
+        q = [self.get_Q(state, a) for a in self.possible_action]
+        return max(q)
 
-    def get_action(self, state):
-        """
-        Choose the best action with Epsilon-Greedy approach
-        """
-        #return highest arm
-        best_Q = [self.get_Q(state, action) for action in self.possible_action]
+    def epsilon_greedy(self, state):
+        # Choose the best action with Epsilon-Greedy approach
+        q = [self.get_Q(state, a) for a in self.possible_action]
+        max_Q = max(q)
 
-        if random.random() > self.epsilon:
-                best_action = random.choice(self.possible_action)
+        if random.random() < self.epsilon: # random move
+            best_action = random.choice(self.possible_action)
 
         else:
-
-            for action in self.possible_action:
-
-                if self.next_state == self.state and self.get_Q(state, action) > max(best_Q):
-                    best_action = action
-                    best_Q = self.possible_action[(state, action)]
-                else:
-                    best_action = random.choice(self.possible_action)
+            if q.count(max_Q) > 1:
+                # Pick an action
+                action = [i for i in range(len(self.possible_action)) if q[i] == max_Q]
+                action_idx = random.choice(action)
+            else:
+                action_idx = q.index(max_Q)
+            best_action = self.possible_action[action_idx]
 
         return best_action
 
-    def update_Q(self, state, action, nextState, reward):
-        """
-        Q-Value update
-        """
-        key = (state, action)
-        if (key not in self.Q_dict):
-            self.Q_dict[key] = 10.0
+    def Q_learning(self, state, action, next_state, reward):
+        getq = self.Q_dict.get(state, action)
+        if getq is None:
+            getq = 10.0
         else:
-            self.Q_dict[key] = self.Q_dict[key] + self.alpha * (reward + self.gamma * self.get_maxQ(nextState) - self.Q_dict[key])
+            self.Q_dict[key] = self.Q_dict[key] + self.alpha*(reward + self.gamma*self.get_maxQ(next_state) - self.Q_dict[key])
 
     def update(self, t):
         # Gather inputs
@@ -85,23 +77,22 @@ class LearningAgent(Agent):
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
 
-        # TODO: Update state
+        # Update state
+        self.next_state = (tuple(inputs.values()), self.next_waypoint)
 
-        #self.next_state = (tuple(inputs.values()), self.next_waypoint)
-        self.state = (inputs['light'],inputs['oncoming'], inputs['left'], self.next_waypoint)
-
-        # TODO: Select action according to your policy
-        action = self.get_action(self.next_state)
+        # Update action
+        next_action = self.epsilon_greedy(self.next_state)
 
         # Execute action and get reward
-        next_reward = self.env.act(self, action)
+        next_reward = self.env.act(self, next_action)
 
-        # TODO: Learn policy based on state, action, reward
+        # Learn policy based on state, action, reward
         if self.reward != None:
-            self.update_Q(self.state, self.action, self.next_state, self.reward)
-
+            # update Q-value
+            self.Q_learn(self.state, self.next_state, self.action, self.reward)
+        # Update stats
         self.state = self.next_state
-        self.action = action
+        self.action = next_action
         self.reward = next_reward
         self.cum_rewards += next_reward
 
@@ -115,14 +106,14 @@ def run():
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
     e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
-    # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
+    # You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
-    # NOTE: To speed up simulation, reduce update_delay and/or set display=False
+    sim = Simulator(e, update_delay=.0001, display=False)  # create simulator (uses pygame when display=True, if available)
+    # To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
-    # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
+    # To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
 
 if __name__ == '__main__':
